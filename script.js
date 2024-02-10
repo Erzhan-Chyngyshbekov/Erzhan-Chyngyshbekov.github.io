bfbcdc9ec48ed4df6a414ac18b64942d2e1f37fb
Erzhan <echynhyshbekov@gmail.com>
2024-02-11 02:30:46 +0600

    yes or no game

---
+++ script.js
@@ -0,0 +1,17 @@
+const random = (min, max) => {
+    const rand = min + Math.random() * (max - min + 1);
+    return Math.floor(rand);
+}
+
+const btn = document.querySelector('#btn');
+btn.addEventListener('mouseenter', () => {
+    btn.style.left = `${random(0, 90)}%`;
+    btn.style.top = `${random(0, 90)}%`;
+});
+
+btn.addEventListener('click', () => {
+    btn.innerHTML = 'Конечно!'
+    btn.style.left = `${random(0, 90)}%`;
+    btn.style.top = `${random(0, 90)}%`;
+});
+
